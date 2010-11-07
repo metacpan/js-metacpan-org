df9e1ca12bb32188597bca24f8b6fe3553172cb6
Mark Jubenville <ioncache@gmail.com>
2010-11-07 07:54:33 -0500

    Search page now can do a basic search on module name.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,1 +1,31 @@
 // main javascript for search.metacpan.org
+
+function metaSearch(value) {
+    if ( value !== '') {
+        $.ajax({
+            url: 'http://api.metacpan.org/module/_search',
+            cache: false,
+            data: {
+                'q': 'name:"' + value + '"'
+            },
+            beforeSend: function() {
+                $("#results_container").fadeOut(200);
+                $("#search_results").html("");
+            },
+            dataType: 'json',
+            success: function(res) {
+                $(res.hits.hits).each(function() {
+                    $("<li />").addClass("search_results").html(this._source.name).appendTo('#search_results');
+                    $("#results_container").fadeIn(200);
+                });
+            },
+            error: function(xhr,status,error) {
+                if ( window.console && window.console.log ) {
+                    console.log(xhr);
+                    console.log(status);
+                    console.log(error);
+                }
+            }
+        });
+    }  
+}
