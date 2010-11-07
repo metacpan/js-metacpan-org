fb8bca24020118237fa9f94c6c1a4bbe8e01539c
Mark Jubenville <ioncache@gmail.com>
2010-11-07 09:52:38 -0500

    A bit of simple formatting and extra information displayed for each module.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,19 +3,30 @@
 function metaSearch(value) {
     if ( value !== '') {
         $.ajax({
+            type: 'get',
             url: 'http://api.metacpan.org/module/_search',
+            data: { 'q': 'name:"' + value + '"' },
+            //data: '{ size: 15, query: { term: { name: "' + value + '" } } }',
+            //data: {
+            //    "query": {
+            //        "term": {
+            //            "name": '"' + value + '"'
+            //        }
+            //    }
+            //},
+            dataType: 'json',
             cache: false,
-            data: {
-                'q': 'name:"' + value + '"'
-            },
             beforeSend: function() {
                 $("#results_container").fadeOut(200);
                 $("#search_results").html("");
             },
-            dataType: 'json',
             success: function(res) {
+                var moduleTemplate = '<div><a href="#" class="name">{{name}}</a><span class="version">{{version}}</span></div><div class="info_container"><a href="#" class="dist">{{dist}}</a><span class="distvname">{{distvname}}</span><a href="#" class="author">{{author}}</a></div>';
                 $(res.hits.hits).each(function() {
-                    $("<li />").addClass("search_results").html(this._source.name).appendTo('#search_results');
+                    var view = $.extend({}, this._source);
+                    var module = Mustache.to_html(moduleTemplate, view);
+                    //console.log(module);
+                    $("<li />").addClass("module_container").html(module).appendTo('#search_results');
                     $("#results_container").fadeIn(200);
                 });
             },
