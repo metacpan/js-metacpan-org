37eff991ed0e949b6f59f484d35f08b594b98e6e
Mark Jubenville <ioncache@gmail.com>
2010-11-09 17:14:18 -0500

    Fixed the widths of the table cells -- perhaps a personal preference but I dislike it when the columns of a table change width from page to page due to differing content sizes.
    Added and ellipsis plugin for cases where cell content is larger than the table cell.  Should gracefully degrade to using the text-overflow css property in browsers that support it.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -17,39 +17,25 @@ function metaSearch(value) {
             dataType: 'json',
             cache: false,
             beforeSend: function() {
-                //$("#paging_container").fadeOut(200);
-                $("#results_table").fadeOut(200);
-                //$("#search_results").html("");
+                $("#results_container").fadeOut(200);
                 $("#search_loader").fadeIn(200);
             },
             success: function(res) {
-                //var moduleTemplate = '<div><a href="#" class="name">{{name}}</a><span class="version">{{version}}</span></div><div class="info_container"><a href="#" class="dist">{{distvname}}</a><a href="#" class="author">{{author}}</a></div>';
-                //$(res.hits.hits).each(function() {
-                //    var view = $.extend({}, this._source);
-                //    var module = Mustache.to_html(moduleTemplate, view);
-                //    $("<li />").addClass("module_container").html(module).appendTo('#search_results');
-                //});
-                //$("#paging_container").pajinate({
-                //    items_per_page: 10,
-                //    num_page_links_to_display: 20
-                //}).fadeIn(200);
                 var rowData = [];
                 $(res.hits.hits).each(function() {
                     rowData.push([
-                        this._source.name,
-                        this._source.version,
-                        this._source.distvname,
-                        this._source.author,
-                        this._score,
-                        '<a href="' + this._source.download_url + '" title="Download Archive" class="ui-state-default" style="width: 16px; height: 16px; border: 0px; margin: 0px; padding: 0px;"><span class="ui-icon ui-icon-disk" style="width: 16px; height: 16px;"></span></a>'
+                        
+                        '<div class="cell_contents" title="' + this._source.name + '" style="width: 219px;">' + this._source.name + '</div>',
+                        '<div class="cell_contents" title="' + this._source.version + '" style="width: 68px;">' + this._source.version + '</div>',
+                        '<div class="cell_contents" title="' + this._source.release_date.substr(0,10) + '" style="width: 68px;">' + this._source.release_date.substr(0,10) + '</div>',
+                        '<div class="cell_contents" title="' + this._source.distvname + '" style="width: 187px;">' + this._source.distvname + '</div>',
+                        '<div class="cell_contents" title="' + this._source.author + '" style="width: 126px;">' + this._source.author + '</div>',
+                        '<div class="cell_contents" title="' + this._source._score + '" style="width: 86px;">' + this._score + '</div>'
                     ]);
                 });
-
-                $("#results_table").dataTable({
-                    aaData: rowData,
-                    bRetrieve: true
-                });
-                $("#results_table").fadeIn(200);
+                $("#results_table").dataTable().fnClearTable();
+                $("#results_table").dataTable().fnAddData(rowData);
+                $("#results_container").fadeIn(200);
                 $("#search_loader").fadeOut(200);
             },
             error: function(xhr,status,error) {
