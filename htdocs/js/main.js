e18193e95d56c5c1f86a121ad841c050d030f164
Mark Jubenville <ioncache@gmail.com>
2010-11-09 11:08:31 -0500

    Changed the display of the search results to use a table instead of a <ul>

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,10 +3,10 @@
 function metaSearch(value) {
     if ( value !== '') {
         $.ajax({
-            type: 'post',
+            type: 'get',
             url: 'http://api.metacpan.org/module/_search',
-            //data: { 'q': 'name:"' + value + '"', 'size': 500 },
-            data: '{ size: 500, query: { term: { name: "' + value + '" } } }',
+            data: { 'q': 'name: "' + value + '"', size: 500 },
+            //data: '{ size: 500, query: { term: { name: "' + value + '" } } }',
             //data: {
             //    "query": {
             //        "term": {
@@ -17,21 +17,39 @@ function metaSearch(value) {
             dataType: 'json',
             cache: false,
             beforeSend: function() {
-                $("#paging_container").fadeOut(200);
-                $("#search_results").html("");
+                //$("#paging_container").fadeOut(200);
+                $("#results_table").fadeOut(200);
+                //$("#search_results").html("");
                 $("#search_loader").fadeIn(200);
             },
             success: function(res) {
-                var moduleTemplate = '<div><a href="#" class="name">{{name}}</a><span class="version">{{version}}</span></div><div class="info_container"><a href="#" class="dist">{{distvname}}</a><a href="#" class="author">{{author}}</a></div>';
+                //var moduleTemplate = '<div><a href="#" class="name">{{name}}</a><span class="version">{{version}}</span></div><div class="info_container"><a href="#" class="dist">{{distvname}}</a><a href="#" class="author">{{author}}</a></div>';
+                //$(res.hits.hits).each(function() {
+                //    var view = $.extend({}, this._source);
+                //    var module = Mustache.to_html(moduleTemplate, view);
+                //    $("<li />").addClass("module_container").html(module).appendTo('#search_results');
+                //});
+                //$("#paging_container").pajinate({
+                //    items_per_page: 10,
+                //    num_page_links_to_display: 20
+                //}).fadeIn(200);
+                var rowData = [];
                 $(res.hits.hits).each(function() {
-                    var view = $.extend({}, this._source);
-                    var module = Mustache.to_html(moduleTemplate, view);
-                    $("<li />").addClass("module_container").html(module).appendTo('#search_results');
+                    rowData.push([
+                        this._source.name,
+                        this._source.version,
+                        this._source.distvname,
+                        this._source.author,
+                        this._score,
+                        '<a href="' + this._source.download_url + '" title="Download Archive" class="ui-state-default" style="width: 16px; height: 16px; border: 0px; margin: 0px; padding: 0px;"><span class="ui-icon ui-icon-disk" style="width: 16px; height: 16px;"></span></a>'
+                    ]);
                 });
-                $("#paging_container").pajinate({
-                    items_per_page: 10,
-                    num_page_links_to_display: 20
-                }).fadeIn(200);
+
+                $("#results_table").dataTable({
+                    aaData: rowData,
+                    bRetrieve: true
+                });
+                $("#results_table").fadeIn(200);
                 $("#search_loader").fadeOut(200);
             },
             error: function(xhr,status,error) {
