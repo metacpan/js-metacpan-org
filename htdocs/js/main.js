a9ec6c059cb0cf41f852396e4a08211ad080d00d
Mark Jubenville <ioncache@gmail.com>
2010-11-10 18:06:23 -0500

    Added display of pod when clicking on a row in the result list.  This currently pulls the pod from search.cpan.org.
    Added some query parameter parsing to allow for auto searcing and showing of pod.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -18,6 +18,7 @@ function metaSearch(value) {
             cache: false,
             beforeSend: function() {
                 $("#results_container").fadeOut(200);
+                $("#module_container").fadeOut(200);
                 $("#search_loader").fadeIn(200);
             },
             success: function(res) {
@@ -47,4 +48,55 @@ function metaSearch(value) {
             }
         });
     }  
+}
+
+function remotePod(module, dist, author) {
+    var modulePath = module.replace(/::/g, '/');
+    var url = 'http://search.cpan.org/~' + escape(author) + '/' + escape(dist) + '/lib/' + escape(modulePath) + '.pm';
+    requestCrossDomain( url, function(resp) {
+        var html = resp;
+        html = html.replace(/\n/gm, 'metacpan_newline');
+        html = html.replace(/^.*<div class="pod">/m, '<div class="pod">');
+        html = html.replace(/<div class="footer">.*$/m, '');
+        html = html.replace(/metacpan_newline/g, '\n');
+        $("#pod_contents").html(html);
+        $("#module_container").fadeIn(200);
+    });
+}
+
+// Accepts a url and a callback function to run.
+function requestCrossDomain( site, callback ) {
+
+    // If no url was passed, exit.
+    if ( !site ) {
+        alert('No site was passed.');
+        return false;
+    }
+
+    // Take the provided url, and add it to a YQL query. Make sure you encode it!
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+
+    // Request that YSQL string, and run a callback function.
+    // Pass a defined function to prevent cache-busting.
+    $.getJSON( yql, cbFunc );
+
+    function cbFunc(data) {
+        // If we have something to work with...
+        if ( data.results[0] ) {
+            // Strip out all script tags, for security reasons.
+            // BE VERY CAREFUL. This helps, but we should do more.
+            data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+    
+            // If the user passed a callback, and it
+            // is a function, call it, and send through the data var.
+            if ( typeof callback === 'function') {
+                callback(data);
+            }
+        }
+        // Else, Maybe we requested a site that doesn't exist, and nothing returned.
+        else if (window.console && window.console.log) {
+            console.log('Error: Nothing returned from getJSON.');
+            callback('');
+        }
+    }
 }
