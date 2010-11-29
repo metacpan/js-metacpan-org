f43b6aaae7afe032dadecb61df694bd965bd5844
Mark Jubenville <ioncache@gmail.com>
2010-11-28 21:30:05 -0500

    Most logic now moved into controler and views.
    History now maintained between pod and results lists.

--- htdocs/js/controllers.js
+++ htdocs/js/controllers.js
@@ -1,14 +1,160 @@
 // Controllers used by search.metacpan.org
 
-var Workspace = Backbone.Controller.extend({
+var Metacpan = Backbone.Controller.extend({
 
-  routes: {
-    "search/:query":        "search",
-    "showpod/:query":       "showpod"
-  },
-  
-  search: function(query) {
+    routes: {
+        "/search/:type/:query":      "search",
+        "/showpod/:query":           "showpod"
+    },
+
+    initialize: function() {
+        _.bindAll(this, [ "hideViews", "showPod", "moduleSearch" ]);
+    },
     
-  }
+    search: function(type, query) {
+        debug("Action: search");
+        debug("Type :  " + type);
+        debug("Query:  " + query);
+
+        this.hideViews();
+
+        switch(type) {
+        
+            case 'module':
+                this.moduleSearch(query);
+                break;
+            default:
+                debug('"' + type + '" is not a valid search type.' );
+        
+        }
+        
+    },
+
+    showpod: function(query) {
+        debug("Action: showpod");
+        debug("Query:  " + query);
+
+        this.hideViews();
+
+        $.ajax({
+            type: 'get',
+            url:  'http://api.metacpan.org/module/' + query,
+            dataType: 'json',
+            beforeSend: function() {
+                $("#results_container").fadeOut(200, function() {
+                    $("#module_container").fadeIn(200);
+                });
+            },
+            success: function(res) {
+                MetacpanController.showPod(res._source.name);
+            },
+            error: function(xhr, error, status) {
+                debug(xhr);
+                debug(status);
+                debug(error);
+                ModuleDetailsView.noPod("no module named << " + query + " >> could be found.");
+            }
+        });
+
+    },
+    
+    showPod: function(module) {
+        this.saveLocation("/showpod/" + module);
+        $.ajax({
+            type: 'get',
+            //type: 'post',
+            url: 'http://api.metacpan.org/pod/' + module,
+            dataType: 'json',
+            cache: false,
+            beforeSend: function() {
+                ModuleDetailsView.resetPod();
+                ModuleDetailsView.show();
+            },
+            success: function(res) {
+                debug(res);
+                if ( res.hasOwnProperty('_source') && res._source.hasOwnProperty('pod') ) {
+                    var podHTML = res._source.pod;
+                    
+                    // /some interim formatting until we clean up the formatted
+                    // pod stored in ElasticSearch
+                    podHTML = podHTML.replace('<html>', '');
+                    podHTML = podHTML.replace('</html>', '');
+                    podHTML = podHTML.replace('<head>', '');
+                    podHTML = podHTML.replace('</head>', '');
+                    podHTML = podHTML.replace('<body>', '');
+                    podHTML = podHTML.replace('</body>', '');
+                    podHTML = podHTML.replace('<title>', '');
+                    podHTML = podHTML.replace('</title>', '');
+                    podHTML = podHTML.replace(/<meta.* \/>/, '');
+                    podHTML = podHTML.replace(/<link.* \/>/, '');
+                    podHTML = podHTML.replace('<div style="height:50px">&nbsp;</div>', '');
+
+                    ModuleDetailsView.updatePod(podHTML);
+
+                } else {
+                    ModuleDetailsView.noPod("no pod could be found.");
+                }
+            },
+            error: function(xhr,status,error) {
+                debug(xhr);
+                debug(status);
+                debug(error);
+                ModuleDetailsView.noPod("no pod could be found.");
+            }
+        });
+    },
+
+    moduleSearch: function(module) {
+        if ( module !== '') {
+            this.saveLocation("/search/module/" + module);
+            $.ajax({
+                type: 'get',
+                //type: 'post',
+                url: 'http://api.metacpan.org/module/_search',
+                data: { 'q': 'name: "' + module + '"', size: 1000 },
+                //data: '{ "size": "500", "query": { "term": { "name": "' + module.toLowerCase() + '" } } }',
+                //data: {
+                //    "query": {
+                //        "term": {
+                //            "name": '"' + module + '"'
+                //        }
+                //    }
+                //},
+                dataType: 'json',
+                cache: false,
+                beforeSend: function() {
+                    MetacpanController.hideViews(function () {
+                        $("#pod_contents").show();
+                        $("#pod_loader").show();
+                        $("#source_contents").hide();
+                        $("#no_pod").hide();
+                        $("#module_results_table").dataTable().fnClearTable(); 
+                    });
+                    $("#search_loader").fadeIn(200);
+                },
+                success: function(res) {
+                    debug(res);
+                    ModuleResultsView.update(res);
+                    ModuleResultsView.show();
+                    $("#search_loader").fadeOut(200);
+                },
+                error: function(xhr,status,error) {
+                    debug(xhr);
+                    debug(status);
+                    debug(error);
+                    ModuleResultsView.show();
+                    $("#search_loader").fadeOut(200);
+                }
+            });
+        }  
+    },
+
+    hideViews: function(callback) {
+        $(".metacpanView").fadeOut(200, function() {
+            if (typeof(callback) != 'undefined' ) {
+                callback();
+            }
+        });
+    }
 
 });
--- htdocs/js/views.js
+++ htdocs/js/views.js
@@ -1,23 +1,182 @@
 // Views used by search.metacpan.org
 
-var views = (function($) {
+var ModuleResults = Backbone.View.extend({
 
-    var my = {};
+    id: "module_results",
 
-    var _views = [];
+    tagName: "div",
 
-    // module results view
-    _views.moduleResults = [];
-    _views.moduleResults.view = {
+    className: "metacpanView",
+
+    el: $("#module_results"),
+
+    initialize: function() {
+        _.bindAll(this, [ "render", "show", "hide", "update" ]);
+    },
+
+    events: {
+        "click #module_results_table tbody tr": "showpod",
+        "hover #module_results_table tbody tr.ui-state-active": "hover",
+        "hover #module_results_table tbody tr.ui-state-default": "hover"
+    },
+
+    render: function() {
+        $(this.el).append(ich.resultsTable({ id: "module_results_table" }));
         
-    };
-    _views.moduleResults.template = '<table id="module_results_table" cellpadding="0" cellspacing="0" border="0" class="display"></table>';
+        $("#module_results_table").dataTable({
+            aoColumns: [
+                { sTitle: '<div class="cell_contents" title="Sort by Module Name" style="width: 219px;">Module</div>', sWidth: '219px' },
+                { sTitle: '<div class="cell_contents" title="Sort by Version" style="width: 68px;">Version</div>', sWidth: '68px', bSearchable: false },
+                { sTitle: '<div class="cell_contents" title="Sort by Release Date" style="width: 68px;">Date</div>', sWidth: '68px' },
+                { sTitle: '<div class="cell_contents" title="Sort by Distribution Name" style="width: 187px;">Distribution</div>', sWidth: '187px' },
+                { sTitle: '<div class="cell_contents" title="Sort by Author ID" style="width: 126px;">Author</div>', sWidth: '126px' },
+                { sTitle: '<div class="cell_contents" title="Sort by Search Score" style="width: 86px;">Score</div>', sWidth: '86px', bSearchable: false }
+            ],
+            aaSorting: [[ 5, "desc" ]],
+            bAutoWidth: false,
+            bJQueryUI: true,
+            fnDrawCallback: function() {
+                var settings = $("#module_results_table").dataTable().fnSettings();
+                if ( $.cookie('tableDisplayLength') !== settings._iDisplayLength ) {
+                    $.cookie('tableDisplayLength', settings._iDisplayLength);
+                }
+                $(".cell_contents:visible").textOverflow();
+            },
+            iDisplayLength: Number($.cookie('tableDisplayLength')),
+            sPaginationType: 'full_numbers',
+            oLanguage: {
+                sSearch: 'Filter:'
+            }
+        });
+    },
 
-    // accessors
-    my.getView = function(name) {
-        //return Mustache.to_html(_views[name].template, _views[name].view);
-    };
+    // shows the pod when clicking on a row
+    showpod: function(row) {
+        var module = $(row.currentTarget).find('td:first-child').text();
+        this.hide(MetacpanController.showPod(module));
+    },
 
-    return my;
+    // toggles row class when hovering
+    hover: function(row) {
+        $(row.currentTarget).toggleClass('ui-state-highlight');
+    },
 
-}(jQuery));
+    // fades the view in
+    show: function(callback) {
+        $(this.el).fadeIn(200, function() {
+            if (typeof(callback) != 'undefined' ) {
+                callback();
+            }
+        });
+    },
+
+    // fades the view out
+    hide: function(callback) {
+        $(this.el).fadeOut(200, function() {
+            if (typeof(callback) != 'undefined' ) {
+                callback();
+            }
+        });
+    },
+
+    // updates the module results table
+    update: function(res) {
+        var rowData = [];
+        $(res.hits.hits).each(function() {
+            rowData.push([
+                '<div class="cell_contents" title="' + this._source.name + '" style="width: 219px;">' + this._source.name + '</div>',
+                '<div class="cell_contents" title="' + this._source.version + '" style="width: 68px;">' + this._source.version + '</div>',
+                '<div class="cell_contents" title="' + this._source.release_date.substr(0,10) + '" style="width: 68px;">' + this._source.release_date.substr(0,10) + '</div>',
+                '<div class="cell_contents" title="' + this._source.distvname + '" style="width: 187px;">' + this._source.distvname + '</div>',
+                '<div class="cell_contents" title="' + this._source.author + '" style="width: 126px;">' + this._source.author + '</div>',
+                '<div class="cell_contents" title="' + this._score + '" style="width: 86px;">' + this._score + '</div>'
+            ]);
+        });
+        $("#module_results_table").dataTable().fnAddData(rowData);
+    }
+
+});
+
+var ModuleDetails = Backbone.View.extend({
+
+    id: "module_details",
+
+    tagName: "div",
+
+    className: "metacpanView",
+
+    el: $("#module_details"),
+
+    initialize: function() {
+        _.bindAll(this, [ "render", "show", "hide", "updatePod", "noPod", "resetPod" ]);
+    },
+
+    render: function() {
+        $(this.el).append(ich.moduleDetailsView());
+        $(".back_to_results").button({
+            icons: { primary: 'ui-icon-arrowreturnthick-1-w' },
+            text: false
+        }).click(function() {
+            ModuleDetailsView.hide(
+                ModuleResultsView.show((function() {
+                    $("#pod_contents").hide();
+                    $("#pod_loader").show();
+                    $("#source_contents").hide();
+                    $("#no_pod").hide();
+                }))
+            );
+        });
+    },
+
+    // fades the view in
+    show: function(callback) {
+        $(this.el).fadeIn(200, function() {
+            if (typeof(callback) != 'undefined' ) {
+                callback();
+            }
+        });
+    },
+
+    // fades the view out
+    hide: function(callback) {
+        $(this.el).fadeOut(200, function() {
+            if (typeof(callback) != 'undefined' ) {
+                callback();
+            }
+        });
+    },
+
+    updatePod: function(podHTML) {
+        $("#pod_contents").html(podHTML).wrapInner('<div class="pod" />');
+        $("#pod_contents a").filter(function() {
+            var href = $(this).attr('href');
+            var http = /^http:\/\//;
+            var anchor = /^#/;
+            var mailto = /^mailto:/;
+            return ( http.exec(href) || anchor.exec(href) || mailto.exec(href) ) ? 0 : 1;
+        }).map(function() {
+            $(this).attr('href', '/#/showpod/' + $(this).attr('href'));
+        });
+        this.$("#pod_contents pre").each(function(i, e) {
+            $(this).addClass("language-perl");
+            $(this).wrapInner('<code />');
+            hljs.highlightBlock(e, '    ');
+        });
+        $("#pod_loader").fadeOut(200, function() {
+            $("#pod_contents").fadeIn(200);
+        });
+    },
+
+    noPod: function(message) {
+        $("#pod_loader").fadeOut(200, function() {
+            $("#pod_error").text(message);
+            $("#no_pod").fadeIn(200);
+        });  
+    },
+
+    resetPod: function() {
+        $("#pod_contents").html('');
+        $("#pod_loader").fadeIn(200);
+    }
+
+});
